b9ed252c0b734615384772fdf4c17727c1153045
Brecko <pierrick.brecko@gmail.com>
2018-06-22 16:19:43 +0200

    Création de la page de connexion interne

---
+++ WinTchat/WinTchat/ConnexionWinTchat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTchat
+{
+    public partial class Connexion_WinTchat : Form
+    {
+        public Connexion_WinTchat()
+        {
+            InitializeComponent();
+        }
+
+        private void Connexion_WinTchat_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
